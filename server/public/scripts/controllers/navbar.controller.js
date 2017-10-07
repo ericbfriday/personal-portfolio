0a93596d2f5863ec068cac72edb133f932f6d929
Eric Friday <ericfriday@gmail.com>
2017-10-07 18:54:21 -0500

    colored grid boxes forming upon page select for about code page

--- server/public/scripts/controllers/navbar.controller.js
+++ server/public/scripts/controllers/navbar.controller.js
@@ -1,16 +1,9 @@
 angular.module('NavBar', ['ngMaterial']) // creates NavBar with ngMaterial Dependecy
     .controller('NavCtrl', navCtrl);
 
-    var vm = this;
+var vm = this;
 
 // provies basic functionality for nav bar
 function navCtrl($scope) {
     vm.currentNavItem = 'home';
-
-    vm.goto = function (page) {
-        console.log("Goto " + page);
-    };
-
-    // console.log('inside navCtrl()!');
-    
 } // end navCtrl()
